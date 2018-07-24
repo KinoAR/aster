72963898a9d47eaa1803d4e615a763081cfc860e
Kino  Rose <kinoarose@gmail.com>
2018-07-23 20:30:47 -0400

    Add controls, add player character boundaries

--- game.js
+++ game.js
@@ -31,34 +31,35 @@ function create() {
   this.livesText = this.add.text(50, 50,  `Lives:  ${this.lives}`);
 
   this.player = this.physics.add.sprite(400, 250, 'ship');
+  this.player.setAngle(90);
   this.player.setCollideWorldBounds(true);
   this.playerBullets = [];
 }
 
 function update() {
   this.input.keyboard.on('keydown_RIGHT', () => {
-    this.ship.body.velocity.x = speed;
+    this.player.body.velocity.x = speed;
   })
   this.input.keyboard.on('keyup_RIGHT', () => {
-    this.ship.body.velocity.x = 0;
+    this.player.body.velocity.x = 0;
   });
   this.input.keyboard.on('keydown_LEFT', () => {
-    this.ship.body.velocity.x = -speed;
+    this.player.body.velocity.x = -speed;
   });
   this.input.keyboard.on('keyup_LEFT', () => {
-    this.ship.body.velocity.x = 0;
+    this.player.body.velocity.x = 0;
   })
   this.input.keyboard.on('keydown_DOWN', () => {
-    this.ship.body.velocity.y = speed;
+    this.player.body.velocity.y = speed;
   });
   this.input.keyboard.on('keyup_DOWN', () => {
-    this.ship.body.velocity.y = 0;
+    this.player.body.velocity.y = 0;
   })
   this.input.keyboard.on('keydown_UP', () => {
-    this.ship.body.velocity.y = -speed;
+    this.player.body.velocity.y = -speed;
   });
   this.input.keyboard.on('keyup_UP', () => {
-    this.ship.body.velocity.y = 0;
+    this.player.body.velocity.y = 0;
   })
 
   this.livesText.setText(`Lives:  ${this.lives}`)
